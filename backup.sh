97561cbe5b4f6d2b1f9ed6ed97329a53fe5bb877
Msangi99 <doniaparoma99@gmail.com>
2026-05-22 12:34:24 +0000

    Add daily DB backup script

---
+++ backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+PROJECT_DIR="/root/opticedge"
+DB_USER="opticedge_user"
+DB_PASS="SecurePassword123!"
+DB_NAME="opticedge"
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+FILENAME="backup_${TIMESTAMP}.sql"
+
+cd "$PROJECT_DIR"
+
+docker compose exec -T mysql mysqldump -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" > "database/sql/$FILENAME"
+
+git add "database/sql/$FILENAME"
+git commit -m "Auto DB backup $TIMESTAMP"
+git push
